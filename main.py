30a3d83a886b96c58db7c82c5becaf7e5c44f9c9
Yusuf Keklik <33743193+keklikyusuf@users.noreply.github.com>
2021-11-02 09:38:49 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import time
 import threading
 from datetime import datetime
 
-
 debugging_active = False
 
 
@@ -13,11 +12,13 @@ class Debug:
     # -------------------------------- Developer Layout --------------------------------#
     # Activate button line to be able to debug whole code related with implemented debug forms
     if debugging_active:
+        logging.basicConfig(level=logging.DEBUG,
+                            format='%(asctime)s | %(name)s | %(levelname)s:%(message)s'
+                                   '\n--------------------------------------------------'
+                                   '----------------------------------')
         logging.basicConfig(level=logging.DEBUG)
     logging.debug("Debug level has been started!")
-    timeout = 100  # can be changed for debugging purposes
     # -------------------------------- Developer Layout --------------------------------#
-    pass
 
 
 class Coin:
@@ -32,33 +33,33 @@ class GUI:
 
     @staticmethod
     def LayoutMenu():
-        menu_def = [
-            ['File', ['Exit']],
-            ['Help', ['Help']],
-            ['About', ['About']]
-        ]
-        return menu_def
+        menu_layout = [['&File', ['&Open     Ctrl-O', '&Save       Ctrl-S', '&Properties', 'E&xit']],
+                       ['&Edit', ['&Paste', ['Special', 'Normal', ], 'Undo'], ],
+                       ['&Toolbar', ['---', 'Command &1', 'Command &2',
+                                     '---', 'Command &3', 'Command &4']],
+                       ['&Help', '&About...'], ]
+        return menu_layout
 
     @staticmethod
     def LayoutIntro():
-        layout_intro = [
+        intro_layout = [
             sg.Text('Personalized coin user interface!', size=(30, 1)), sg.Text("", size=(10, 1)),
         ]
-        return layout_intro
+        return intro_layout
 
     @staticmethod
     def LayoutModeSelection():
-        layout_mode_selection = [
+        mode_selection_layout = [
             sg.Radio('Get Instant Price', "RADIO1", key='Instant_Price_Radio', default=True),
             sg.Radio('Create a wallet', "RADIO1", key='Wallet_Radio'),
             sg.Radio('Buy/Sell Tracker', "RADIO1", key='Tracker_Radio'),
             sg.Button('Apply', size=(10, 1))
         ]
-        return layout_mode_selection
+        return mode_selection_layout
 
     @staticmethod
     def LayoutInstantPrice():
-        layout_instant_price = [
+        instant_price_layout = [
             sg.Frame(layout=[
                 [sg.Text('Coin Instant Price Tracker is selected!', size=(73, 1))],
                 [sg.Text("Name your coin: ", size=(12, 1)),
@@ -69,19 +70,16 @@ class GUI:
                  sg.InputText(key='Instant_Price_Coin_Following_Time', size=(10, 1)),
                  sg.Button('Follow', size=(12, 1))
                  ],
-                [sg.Text("Time: ", size=(6, 1), font=("Helvetica", 11)),
-                 sg.Text("", key="Following_Time", size=(10, 1), font=("Helvetica", 11)),
-                 sg.Text("Coin name is: ", size=(10, 1), font=("Helvetica", 11)),
-                 sg.Text("", key="Following_Name", size=(10, 1), font=("Helvetica", 11)),
-                 sg.Text("Coin price is: ", size=(10, 1), font=("Helvetica", 11)),
-                 sg.Text("", key="Following_Price", size=(12, 1), font=("Helvetica", 11)),
-                 ]], title='Coin Instant Price ')
+                [sg.Multiline(size=(105, 10), disabled=True, auto_refresh=True, reroute_cprint=False,
+                              write_only=True, autoscroll=False, justification='l',
+                              key='Layout_Instant_Price')]
+            ], title='Coin Instant Price ')
         ]
-        return layout_instant_price
+        return instant_price_layout
 
     @staticmethod
     def LayoutCreateWallet():
-        layout_create_wallet = [
+        create_wallet_layout = [
             sg.Frame(layout=[
                 [sg.Text('Coin Instant Price Tracker is selected!', size=(73, 1))],
                 [sg.Text("Name your coin: ", size=(12, 1)),
@@ -92,13 +90,16 @@ class GUI:
                  sg.InputText(key='Create_Wallet_Coin_Following_Time', size=(10, 1)),
                  sg.Button('Create', size=(12, 1))
                  ],
-                [sg.Output(size=(90, 4), visible=True)]], title='Create a Wallet (Future Part) ')
+                [sg.Multiline(size=(105, 10), disabled=True, auto_refresh=True, reroute_cprint=False,
+                              write_only=True, autoscroll=False, justification='l',
+                              key='Layout_Create_Wallet')],
+            ], title='Create a Wallet (Future Part) ')
         ]
-        return layout_create_wallet
+        return create_wallet_layout
 
     @staticmethod
     def LayoutBuySellTracker():
-        layout_buy_sell_tracker = [
+        buy_sell_tracker_layout = [
             sg.Frame(layout=[
                 [sg.Text('Coin Instant Price Tracker is selected!', size=(73, 1))],
                 [sg.Text("Name your coin: ", size=(12, 1)),
@@ -109,9 +110,39 @@ class GUI:
                  sg.InputText(key='Buy_Sell_Tracker_Coin_Following_Time', size=(10, 1)),
                  sg.Button('Track', size=(12, 1))
                  ],
-                [sg.Output(size=(90, 4), visible=True)]], title='Buy/Sell Tracker (Future Part)')
+                [sg.Multiline(size=(105, 10), disabled=True, auto_refresh=True, reroute_cprint=False,
+                              write_only=True, autoscroll=False, justification='l',
+                              key='Layout_Buy_Sell_Tracker')]
+            ], title='Buy/Sell Tracker (Future Part)')
         ]
-        return layout_buy_sell_tracker
+        return buy_sell_tracker_layout
+
+    @staticmethod
+    def UpdateLayoutInstantPrice():
+        logging.debug("Instant price radio has been selected!")
+        logging.debug("--------------------------------------")
+        window.Element('layout_instant_price').Update(visible=True)
+        window.Element('layout_create_wallet').Update(visible=False)
+        window.Element('layout_buy_sell_tracker').Update(visible=False)
+        return None
+
+    @staticmethod
+    def UpdateLayoutCreateWallet():
+        logging.debug("Create a wallet radio has been selected!")
+        logging.debug("--------------------------------------")
+        window.Element('layout_instant_price').Update(visible=False)
+        window.Element('layout_create_wallet').Update(visible=True)
+        window.Element('layout_buy_sell_tracker').Update(visible=False)
+        return None
+
+    @staticmethod
+    def UpdateLayoutBuySellTracker():
+        logging.debug("Buy/Sell radio has been selected!")
+        logging.debug("--------------------------------------")
+        window.Element('layout_instant_price').Update(visible=False)
+        window.Element('layout_create_wallet').Update(visible=False)
+        window.Element('layout_buy_sell_tracker').Update(visible=True)
+        return None
 
     @staticmethod
     def LayoutMain():
@@ -123,40 +154,39 @@ class GUI:
                 sg.Column([GUI.LayoutModeSelection()], visible=True, key="layout_mode_selection"),
             ],
             [
-                sg.Menu(GUI.LayoutMenu, tearoff=False)
+                sg.Menu(GUI.LayoutMenu(), tearoff=True)
             ],
             [
                 sg.Column([GUI.LayoutInstantPrice()], visible=False, key="layout_instant_price"),
-                # sg.Column([GUI.LayoutCreateWallet()], visible=False, key="layout_create_wallet"),
-                # sg.Column([GUI.LayoutBuySellTracker()], visible=False, key="layout_buy_sell_tracker"),
+                sg.Column([GUI.LayoutCreateWallet()], visible=False, key="layout_create_wallet"),
+                sg.Column([GUI.LayoutBuySellTracker()], visible=False, key="layout_buy_sell_tracker"),
             ],
         ]
-        window = sg.Window('Window Title', main_layout)
-        return window
+        return main_layout
 
     @staticmethod
     def CreateWindow():
-        window = sg.Window('Welcome to Personalized Coin Follower', GUI.LayoutMain(), default_element_size=(40, 1),
-                           icon=r'app.ico')
-        return window
-
-    @staticmethod
-    def basicWindow():
-        layout = [[sg.Text('My one-shot window.')],
-                  [sg.InputText()],
-                  [sg.Submit(), sg.Cancel()]]
-        window = sg.Window('Window Title', layout)
-
-        print('Basic window has been called!')
-        return window
+        created_window = sg.Window('Welcome to Personalized Coin Follower', GUI.LayoutMain(),
+                                   default_element_size=(40, 1),
+                                   icon=r'app.ico')
+        return created_window
 
 
 if __name__ == '__main__':
-    App = GUI(theme='Reddit')
-    gui = GUI.LayoutMain()
+    app = GUI(theme='Dark')
+    window = GUI.CreateWindow()
     while True:
-        event, values = gui.Read()
+        event, values = window.Read()
         logging.debug(values)
-        print(values)
         if event in (None, 'Cancel', 'Exit'):
             break
+
+        if event == "Apply":
+            logging.info("Apply event has been selected!")
+            if values["Instant_Price_Radio"]:
+                app.UpdateLayoutInstantPrice()
+            if values["Wallet_Radio"]:
+                app.UpdateLayoutCreateWallet()
+            if values["Tracker_Radio"]:
+                app.UpdateLayoutBuySellTracker()
+
